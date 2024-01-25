bd2a702bd1b652c22163019282c0acdb571742e1
meetingmindsexperts <admin@meetingmindsdubai.com>
2024-01-25 23:17:16 +0400

    pagination number count updated to accommodate 150

--- views/view-event-table-data.php
+++ views/view-event-table-data.php
@@ -77,9 +77,10 @@ if ($eventName !== '') {
                 <label for='itemsPerPage'>Items per page:</label>
                 <select id='itemsPerPage' onchange='updatePagination()'>
                     <option value='10'>10</option>
-                    <option value='20'>20</option>
-                    <option value='30'>30</option>
+                    <option value='25'>25</option>
                     <option value='50'>50</option>
+                    <option value='100'>100</option>
+                    <option value='150'>150</option>
                 </select>
               </div>";
                
